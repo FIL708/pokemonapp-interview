fcfc1a7f236a1da9572b81a8ae6556b611f5a00c
FIL708 <filip.zebrowski4013@gmail.com>
2022-10-07 09:06:05 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const pokemonList = document.getElementById("pokemon-list")
+
+fetch("https://pokeapi.co/api/v2/pokemon")
+    .then((res) => res.json())
+    .then((data) => {
+        const pokemonNameArray = data.results;
+
+        pokemonNameArray.forEach(element => {
+            let pokemonListElement = document.createElement("li");
+            pokemonListElement.textContent = element.name;
+            pokemonList.appendChild(pokemonListElement);
+            pokemonListElement.addEventListener("click", fetchDetailData)
+        });   
+    })
+
+const fetchDetailData = (event) => {
+    const url = `https://pokeapi.co/api/v2/pokemon-species/${event.target.textContent.toLowerCase()}`
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+}
+
+let imageUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/ditto.png"
