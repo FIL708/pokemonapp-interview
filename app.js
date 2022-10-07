0595ccbc84608ee99d1ed127279ce8f3bfb0fb73
FIL708 <filip.zebrowski4013@gmail.com>
2022-10-07 11:16:00 +0200

    adding functionality

--- app.js
+++ app.js
@@ -3,22 +3,56 @@ const pokemonList = document.getElementById("pokemon-list")
 fetch("https://pokeapi.co/api/v2/pokemon")
     .then((res) => res.json())
     .then((data) => {
+        
         const pokemonNameArray = data.results;
 
-        pokemonNameArray.forEach(element => {
+        pokemonNameArray.forEach(item => {
             let pokemonListElement = document.createElement("li");
-            pokemonListElement.textContent = element.name;
+            pokemonListElement.textContent = item.name;
             pokemonListElement.classList.add("pokemon-name")
             pokemonList.appendChild(pokemonListElement);
             pokemonListElement.addEventListener("click", fetchDetailData)
         });   
     })
 
+const detailId = document.querySelector(".detail-id")
+const detailName = document.querySelector(".detail-name")
+const detailTypes = document.querySelector(".detail-types")
+const detailImage = document.querySelector(".detail-image")
+const detailStats = document.querySelectorAll(".detail-stat")
+const detailEvo = document.querySelector(".detail-evolution")
+
 const fetchDetailData = (event) => {
-    const url = `https://pokeapi.co/api/v2/pokemon-species/${event.target.textContent.toLowerCase()}`
+    const url = `https://pokeapi.co/api/v2/pokemon/${event.target.textContent.toLowerCase()}`
     fetch(url)
         .then((res) => res.json())
-        .then((data) => console.log(data))
+        .then((data) => {
+            console.log(data.types)
+            //ADD ID OF POKEMON
+            if (data.id < 10) {
+                detailId.textContent = `#00${data.id}`
+            } else if (data.id < 100 && data.id >= 10) {
+                detailId.textContent = `#0${data.id}`
+            } else {
+                detailId.textContent = `#${data.id}`
+            }
+            //ADD NAME OF POKEMON
+            detailName.textContent = data.species.name
+
+            //RENDER TYPES OF POKEMON
+            detailTypes.innerHTML = ""
+            data.types.forEach(item => {
+                console.log(item.type.name);
+                const typeToRender = document.createElement("p")
+                typeToRender.classList.add("detail-type")
+                typeToRender.textContent = item.type.name
+                detailTypes.appendChild(typeToRender)
+            });
+
+            //RENDER IMAGE OF POKEMON
+            detailImage
+
+        })
 }
 
 // let imageUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/ditto.png"
